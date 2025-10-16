a980f609c71fe9e0e0502f869df40bab15ec39ca
minju00-sm <m2nzoo@sookmyung.ac.kr>
2025-10-16 16:09:38 +0900

    07_6-1

--- main.c
+++ main.c
@@ -3,28 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	
-void sub(void);
+int all_files;
 
-int main(int argc, char *argv[])
+extern void sub();
+
+int main(void)
 {
-	int i;
-	for (i=0; i<3; i++)
-		sub();
-		
+	sub();
+	printf("%d\n", all_files);
+	
 	system("pause");
 	return 0;
-	}
-
-void sub(void)
-{
-	int auto_count=0; //자동할당이라 한번 돌면 소멸돼서 0으로 초기화됨. 
-	static int static_count=0; //메모리가 소멸되지 않고 누적되어 출력됨. 
-	
-	auto_count++;
-	static_count++;
-	
-	printf("auto_count=%d\n", auto_count);
-	printf("static_count=%d\n", static_count);
 }
-
